2ec069ea05519142cd27eea145cc82fa60bbe6a0
BasmaAbdElzaher <basmaabdelzaher@gmail.com>
2022-10-03 21:41:56 +0200

    setups

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { FaAirbnb } from 'react-icons/fa';
 
 function App() {
   return (
     <div>
-      Hello, World
+      <FaAirbnb size="3em" className='react-icons-style'/>
+      <h1>Hello, SASS</h1>
+      <button tybe='button' className='btn btn-primary'>Primary</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import './style/main.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
